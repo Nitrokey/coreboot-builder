fd67a1d399787e2133a25afe92607b820ca91c81
Markus Meissner <coder@safemailbox.de>
2023-04-12 22:52:11 +0200

    nitropad: use v1.6.0

--- nitropad-nv41.mk
+++ nitropad-nv41.mk
@@ -1,7 +1,7 @@
 
-COREBOOT_REF = novacustom_nv4x_adl_v1.4.0 
+COREBOOT_REF = novacustom_nv4x_adl_v1.6.0
 
-coreboot/build/coreboot.rom: coreboot/bootsplash.jpg coreboot/configs/defconfig coreboot/util/crossgcc/xgcc blobs-update
+coreboot/build/coreboot.rom: coreboot/configs/defconfig coreboot/util/crossgcc/xgcc blobs-update
 	
 	cp blobs/nitropad-nv41/*.bin coreboot/
 	cp blobs/common/bootsplash-1080.bmp coreboot/bootsplash.bmp
