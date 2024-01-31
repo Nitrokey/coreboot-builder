321127dd73057ceda075e1da02b24e8be38f0d88
Markus Meissner <coder@safemailbox.de>
2024-01-31 23:44:21 +0100

    update nitrowall-pro

--- nitrowall-pro.mk
+++ nitrowall-pro.mk
@@ -1,5 +1,7 @@
 
-COREBOOT_REF = 9f221f6566e7bd3d1170903a4211a0ecc39c564c 
+COREBOOT_REF = 0895a061d5fd356c0a951954d4714265bbbc04b8
+#dasharo
+#9f221f6566e7bd3d1170903a4211a0ecc39c564c 
 # ref: tags/protectli_vault_kbl_v1.0.14
 
 coreboot/build/coreboot.rom: coreboot/bootsplash.jpg coreboot/configs/defconfig coreboot/util/crossgcc/xgcc blobs-update
