6dcafd9df44bb36a0ac099891b8c703cf6dd259a
Markus Meissner <coder@safemailbox.de>
2024-02-01 01:13:46 +0100

    update nitrowall to cb 4.22

--- nitrowall.mk
+++ nitrowall.mk
@@ -1,5 +1,8 @@
 
-COREBOOT_REF = 7f69d690d22efb4d0c9e79acd94bf138a3a81b47 
+COREBOOT_REF = 4.22
+# 6c05f0de191afb35781896da00473dc24e048e48
+# 0895a061d5fd356c0a951954d4714265bbbc04b8
+# 7f69d690d22efb4d0c9e79acd94bf138a3a81b47 
 # ref: tags/4.16
 
 coreboot/build/coreboot.rom: coreboot/bootsplash.bmp coreboot/configs/defconfig coreboot/util/crossgcc/xgcc blobs-update
