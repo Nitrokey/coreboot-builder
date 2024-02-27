d7b08b9513cbbd27aa796fd1ec83d95401c9c813
Markus Meissner <coder@safemailbox.de>
2024-02-27 14:45:39 +0100

    add nitropc-v2

--- nitropc-v2.mk
+++ nitropc-v2.mk
@@ -3,7 +3,7 @@ COREBOOT_REF = 4.22
 
 coreboot/build/coreboot.rom: coreboot/bootsplash.bmp coreboot/configs/defconfig coreboot/util/crossgcc/xgcc blobs-update
 	rm coreboot/src/mainboard/purism/librem_cnl/variants/librem_mini/overridetree.cb
-	cp devicetree.cb coreboot/src/mainboard/purism/librem_cnl/variants/librem_mini/overridetree.cb
+	cp devicetree-v2.cb coreboot/src/mainboard/purism/librem_cnl/variants/librem_mini/overridetree.cb
 	cp gpio.c coreboot/src/mainboard/purism/librem_cnl/variants/librem_mini/gpio.c
 	cp blobs/nitropc/*	coreboot/
 	
