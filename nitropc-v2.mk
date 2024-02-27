c56534025b31f37d6dd070ae83dde53786a01a66
Markus Meissner <coder@safemailbox.de>
2024-02-27 14:58:23 +0100

    nitropc-v2: leave out gpio.c for now

--- nitropc-v2.mk
+++ nitropc-v2.mk
@@ -4,7 +4,7 @@ COREBOOT_REF = 4.22
 coreboot/build/coreboot.rom: coreboot/bootsplash.bmp coreboot/configs/defconfig coreboot/util/crossgcc/xgcc blobs-update
 	rm coreboot/src/mainboard/purism/librem_cnl/variants/librem_mini/overridetree.cb
 	cp devicetree-v2.cb coreboot/src/mainboard/purism/librem_cnl/variants/librem_mini/overridetree.cb
-	cp gpio.c coreboot/src/mainboard/purism/librem_cnl/variants/librem_mini/gpio.c
+	#cp gpio.c coreboot/src/mainboard/purism/librem_cnl/variants/librem_mini/gpio.c
 	cp blobs/nitropc/*	coreboot/
 	
 	make -C coreboot CPUS=$(CPU_COUNT)
