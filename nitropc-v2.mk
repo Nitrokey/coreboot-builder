cbf03af68e00f5c142b14cd05effe77dda1a81b5
Markus Meissner <coder@safemailbox.de>
2024-03-07 16:05:17 +0100

    nitropc-v2: enable auto-reboot on fail

--- nitropc-v2.mk
+++ nitropc-v2.mk
@@ -4,8 +4,9 @@ COREBOOT_REF = 4.22
 coreboot/build/coreboot.rom: coreboot/bootsplash.bmp coreboot/configs/defconfig coreboot/util/crossgcc/xgcc blobs-update
 	rm coreboot/src/mainboard/purism/librem_cnl/variants/librem_mini/overridetree.cb
 	cp devicetree-v2.cb coreboot/src/mainboard/purism/librem_cnl/variants/librem_mini/overridetree.cb
-	#cp gpio.c coreboot/src/mainboard/purism/librem_cnl/variants/librem_mini/gpio.c
+	cp gpio.c coreboot/src/mainboard/purism/librem_cnl/variants/librem_mini/gpio.c
 	cp cmos.default coreboot/src/mainboard/purism/librem_cnl/variants/librem_mini/
+	cp cmos.layout coreboot/src/mainboard/purism/librem_cnl/variants/librem_mini/
 	cp blobs/nitropc/*	coreboot/
 	cp -r blobs/common coreboot/common-blobs
 	
