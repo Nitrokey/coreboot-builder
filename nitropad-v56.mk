bf6ac8b19827bab7fb104b6452643432eebc417e
alexgithublab <alexandre@nitrokey.com>
2024-08-02 15:33:44 +0200

    default config files

---
+++ nitropad-v56.mk
@@ -0,0 +1,11 @@
+
+COREBOOT_REF = novacustom_v56x_mtl_v0.9.0
+
+coreboot/build/coreboot.rom: coreboot/configs/defconfig coreboot/util/crossgcc/xgcc blobs-update
+	
+	cp blobs/nitropad-v56/*.bin coreboot/
+	cp blobs/common/bootsplash-1080.bmp coreboot/bootsplash.bmp
+	cd coreboot && git checkout $(COREBOOT_REF)
+
+	make -C coreboot CPUS=$(CPU_COUNT)
+
