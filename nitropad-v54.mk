9977e635cbfef5ac61176a7e24d6c534591b04ae
alexgithublab <alexandre@nitrokey.com>
2024-08-05 12:21:24 +0200

    add V5x

---
+++ nitropad-v54.mk
@@ -0,0 +1,10 @@
+
+COREBOOT_REF = novacustom_v54x_mtl_v0.9.0
+
+coreboot/build/coreboot.rom: coreboot/configs/defconfig coreboot/util/crossgcc/xgcc blobs-update
+
+	cp blobs/nitropad-v54/*.bin coreboot/
+	cp blobs/common/bootsplash-1080.bmp coreboot/bootsplash.bmp
+	cd coreboot && git checkout $(COREBOOT_REF)
+
+	make -C coreboot CPUS=$(CPU_COUNT)
